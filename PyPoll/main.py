c1310fd5cb0309bd5220cff5a1e15b69e12b15bf
Andrea Burson <andreaburson@Andreas-MacBook-Pro.local>
2020-12-31 11:05:34 -0500

    checking files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,5 @@
 import os
 import csv
-csvpath = os.path.join('Resources', 'election_data.csv')
+csvpath = os.path.join ('Resources', 'election_data.csv')
 
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    for row in csvreader:
-        
+print('hello world')
